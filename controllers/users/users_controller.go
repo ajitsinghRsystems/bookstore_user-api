7fb6da2027df3468f7ddedffca893238c3e6aab9
Ajit Singh <ajit.singh@rsystems.com>
2022-04-13 15:50:10 -0400

    Second Checkin

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,7 +1,12 @@
 package users
 
 import (
+	"bookstore_user-api/domain/users"
+	"bookstore_user-api/services"
+	"bookstore_user-api/utils/errors"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,8 +14,35 @@ var(
 	counter int
 )
 func GetUser(c *gin.Context){
-	c.String(http.StatusNotImplemented, "Implement Me")
+	userID,userErr:=strconv.ParseInt(c.Param("user_id"),10,64)
+	if userErr !=nil{
+		err:= errors.NewBadRequestError("User id should be number")
+		c.JSON(err.Status,err)
+		return
+	}
+	user,getErr :=services.GetUser(userID)
+	if getErr !=nil{
+		c.JSON(getErr.Status,getErr)
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+
 }
 func CreateUsers( c *gin.Context){
-	c.String(http.StatusNotImplemented, "Implement Me")
+	var user users.User
+	fmt.Println(user)
+	if err:=c.ShouldBindJSON(&user); err != nil{
+		
+		restErr := errors.NewBadRequestError("Invalid JSON Body")
+		c.JSON(restErr.Status,restErr)
+
+		return
+	}
+	result,saveErr :=services.CreateUser(user)
+	if saveErr !=nil{
+		c.JSON(saveErr.Status,saveErr)
+		return
+	}
+	
+	c.JSON(http.StatusCreated, result)
 }
